2cfebb15775f80a7a0aa82f3826ebc40ea2d6151
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2022-01-25 13:17:56 +0100

    project is done

---
+++ app.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    window.alert ("Takip edelim lütfen :D")
+    window.location.href="https://www.linkedin.com/in/mustafa-altuntas/";
+    // window.open("https://www.linkedin.com/in/mustafa-altuntas/" );
+  }
+  var q = document.getElementById("search-text");
+  function myFunction2() {
+      window.open('https://www.google.com/search?q='+ q.value );      
+  }
+
+  // function türkce() {
+  //   document.getElementById("about").innerHTML = "hakkında";
+  //    window.open("http://127.0.0.1:5500/solution/index.html");   
+      
+  // }
